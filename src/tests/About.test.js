8c247061f64c2e56d726b64dfa1560f2fee13529
Felipe <luizbrutante@gmail.com>
2022-09-19 15:25:59 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,21 @@
-test('', () => { });
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../pages/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste About', () => {
+  test('Teste h2', () => {
+    renderWithRouter(<About />);
+    const h2 = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
+    expect(h2).toBeInTheDocument();
+  });
+  test('Teste imagem', () => {
+    renderWithRouter(<About />);
+    const image = screen.getByRole('img');
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute(
+      'src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
+});
