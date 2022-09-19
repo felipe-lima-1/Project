48ef2da272e7057df00861d1e44a9192659815d4
Felipe <luizbrutante@gmail.com>
2022-09-19 15:31:19 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('teste FavoritePokemon', () => {
+  test('teste not found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+});
