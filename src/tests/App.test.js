54aecdb7e16eeb2f3c2c7d88edddd59d2fbe48af
Felipe <luizbrutante@gmail.com>
2022-09-19 14:59:25 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste App', () => {
+  test('Teste se home, about e favorites sao redirecionadas', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: 'Home' });
+    const about = screen.getByRole('link', { name: 'About' });
+    const favorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favorite).toBeInTheDocument();
+  });
+});
